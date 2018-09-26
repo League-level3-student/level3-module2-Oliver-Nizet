7aafd3241d137192e03f161cf6aeca429fd92e29
League Student <league@iMac-4.attlocal.net>
2018-09-25 17:18:44 -0700

    Finished Sorting Methods

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -22,7 +22,7 @@ public class BogoSorter extends Sorter {
 	void sort(int[] array, SortingVisualizer display) {
 		boolean sorted = false;
 		Random r = new Random();
-		while(sorted = false){
+		while(sorted == false){
 			int score = 0;
 			for (int i = 0; i < array.length-1; i++) {
 				if(array[i]<array[i+1]){
